563fff259e64540c6650664b73568b5a8590f00b
Sibaru1217 <leehakbin1217@gmail.com>
2025-03-14 16:53:15 +0900

    5. 머지 테스트

--- src/com/msa2025/Hello.java
+++ src/com/msa2025/Hello.java
@@ -3,8 +3,10 @@ package com.msa2025;
 public class Hello {
 
 	public static void main(String[] args) {
-	System.out.println("Hello java");
-System.out.println("브라우저에서 수정");
+		System.out.println("Hello java");
+		System.out.println("브라우저에서 수정");
+		System.out.println("로컬에서 수정");
+		
 	}
 
 }
